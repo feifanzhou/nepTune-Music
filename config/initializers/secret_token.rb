ae5a04528f5b5a3d6a81cca9e32e899a254eac2b
Feifan Zhou <fz75@cornell.edu>
2013-05-21 17:53:17 -0400

    Commit basic Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeptuneMusic::Application.config.secret_token = '22dcb1c39007a9b5079b3008bd63ffdfc5672a318037031dda51b527e5e7e0fadc280a87c94f4fa8f5b52eacea27633d78bad7d8f77914328e89021c0e8a9d25'
