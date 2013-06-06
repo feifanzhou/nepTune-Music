27d4bdb135b5fdc6a5358707640973b2cd5e027d
Feifan Zhou <fz75@cornell.edu>
2013-06-06 16:49:24 -0400

    Add background image to home page, compress all images

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_filter :set_user
+
   def set_user
     if cookies[:new_user]
       @user = User.find_by_id(cookies[:new_user])
@@ -15,35 +17,35 @@ class StaticPagesController < ApplicationController
     @taglines = ["Let's make music", "A world of music"]
     @tagline = @taglines[rand(@taglines.length)]
     
-    set_user
+    # set_user
     # @has_new_user = session[:did_create_new_user]
   end
 
   def market
-    set_user
+    # set_user
   end
 
   def team
-    set_user
+    # set_user
   end
   
   def news
-    set_user
+    # set_user
   end
   
   def careers
-    set_user
+    # set_user
   end
   
   def contact
-    set_user
+    # set_user
   end
   
   def beta
-    set_user
+    # set_user
   end
   
   def terms
-    set_user
+    # set_user
   end
 end
