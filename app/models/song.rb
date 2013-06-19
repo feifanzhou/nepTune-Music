ef6b9511d62b17009fe01dc33c6b8006b8e4938e
Feifan Zhou <feifan@me.com>
2013-06-18 22:38:56 -0400

    Stuff for artist page…should not be in this branch…

---
+++ app/models/song.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Song < ActiveRecord::Base
+  attr_accessible :name
+
+  belongs_to :artist
+  has_one :image
+  belongs_to :album
+
+  def image
+  	return super || self.album.image || nil  # super reads attribute :image
+  end
+end
