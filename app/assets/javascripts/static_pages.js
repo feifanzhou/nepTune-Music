336344e1ba0d47e7362415ead057b91270434ee0
Feifan Zhou <feifan@me.com>
2013-06-24 19:00:11 -0400

    Try to fix static pages JS

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,6 +1,7 @@
 /* Place all the behaviors and hooks related to the matching controller here.
    All this logic will automatically be available in application.js. */
 
+
 window.mobilecheck = navigator.userAgent.match(/(iPhone|iPod|Android|BlackBerry)/);
 
 $(function() {
