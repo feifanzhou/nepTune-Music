1ddfc99325c4f54e7d2524cd2147536950833d8b
Feifan Zhou <feifan@me.com>
2013-07-02 10:47:35 -0400

    Tweak specs

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
 
 describe ArtistsController do
+
 	before :each do
 		@artist = Artist.first
 	end
 	describe "GET #about" do
-		it "finds artist by artistname" do
-			visit artist_about_path(@artist.artistname); save_page
-			puts "2 ===================== #{ artist_about_path(@artist.artistname) }"
-			# assigns(:artist).should eq(@artist)
-			assigns[:artist].should_not be_nil
-		end
 		it "renders the :about view" do
 			visit artist_about_path(@artist.artistname)
 			# response.should render_template :about
@@ -19,11 +14,6 @@ describe ArtistsController do
 	end
 
 	describe "GET #music" do
-		it "finds artist by artistname" do
-			visit artist_music_path(@artist.artistname)
-			# assigns(:artist).should eq(@artist)
-			assigns[:artist].should_not be_nil
-		end
 		it "renders the :music view" do
 			visit artist_music_path(@artist.artistname)
 			# response.should render_template :music
@@ -32,17 +22,6 @@ describe ArtistsController do
 	end
 
 	describe "GET #events" do
-		it "finds artist by artistname" do
-			visit artist_events_path(@artist.artistname)
-			# assigns(:artist).should eq(@artist)
-			assigns[:artist].should_not be_nil
-		end
-		it "gets all the artist's events" do
-			# evt = create_artist_event(artist)
-			# visit artist_events_path(artist)
-			# assigns(:events).should eq([evt])
-			pending
-		end
 		it "renders the :events view" do
 			# response.should render_template :events
 			response.should be_success
