d8035efab4ddeb4c4b4415a38dc35f5d9db6d54f
Feifan Zhou <feifan@me.com>
2013-07-01 20:11:48 -0400

    Update FactoryGirl to 4.1

--- Gemfile
+++ Gemfile
@@ -36,7 +36,8 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
-  gem 'factory_girl_rails', '1.0'
+  # gem 'factory_girl_rails', '1.0'
+  gem 'factory_girl_rails', '4.1.0'
   gem 'faker'
   gem 'rb-fsevent', '0.9.3'
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,17 @@
-Factory.define :artist do |a|
-	a.artistname { 'paulbunyan' }
-end
+FactoryGirl.define do
+	factory :artist do |a|
+		a.artistname { 'paulbunyan' }
+	end
 
-Factory.define :contact_info do |c|
-	c.email { 'paul@bunyan.com' }
-	c.phone { '(401) 123-4567' }
-	c.website { 'paulbunyan.com' }
-end
+	factory :contact_info do |c|
+		c.email { 'paul@bunyan.com' }
+		c.phone { '(401) 123-4567' }
+		c.website { 'paulbunyan.com' }
+	end
 
-Factory.define :event do |e|
-	e.name { (0...50).map{ ('a'..'z').to_a[rand(26)] }.join }
-	e.start_at { DateTime.new(2013, 6, 7, 16, 0, 0, '-4') }
-	e.end_at { DateTime.new(2013, 6, 7, 16, 0, 0, '-4').since(3600) }
+	factory :event do |e|
+		e.name { (0...50).map{ ('a'..'z').to_a[rand(26)] }.join }
+		e.start_at { DateTime.new(2013, 6, 7, 16, 0, 0, '-4') }
+		e.end_at { DateTime.new(2013, 6, 7, 16, 0, 0, '-4').since(3600) }
+	end
 end
