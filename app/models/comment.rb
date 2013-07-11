73da9949f3d4ba5da1a18037c944bd10794ebae5
Pierre Karashchuk <krchtchk@gmail.com>
2013-07-11 14:31:30 -0400

    annotate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,22 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  text       :string(255)
+#  upvotes    :integer
+#  location   :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ActiveRecord::Base
   attr_accessible :text, :upvotes, :location, :user
   after_initialize :defaults
 
   belongs_to :user
-  has_many :media, as :media_holder
+  has_many :media, as: :media_holder
 
   validates :user, presence: true
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  text       :string(255)
+#  upvotes    :integer
+#  location   :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Comment do
