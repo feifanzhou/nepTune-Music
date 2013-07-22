27c0da59cc3f727b38df2b4037f19b311af25f11
Feifan Zhou <feifan@me.com>
2013-07-22 14:14:31 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130722164517) do
+ActiveRecord::Schema.define(:version => 20130722172407) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20130722164517) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "route"
   end
 
   create_table "attendees", :force => true do |t|
@@ -91,8 +92,10 @@ ActiveRecord::Schema.define(:version => 20130722164517) do
   end
 
   create_table "influences", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "artist_id"
+    t.integer  "influence_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "media", :force => true do |t|
