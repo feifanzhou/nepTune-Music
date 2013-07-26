92f41a13f1fbe63e058d24f98c9c458a50e1ddda
Pierre Karashchuk <krchtchk@gmail.com>
2013-07-26 12:22:12 -0400

    try to fix pause button

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -9,7 +9,7 @@ soundManager.setup({
     },
     ontimeout: function() {
         // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
-        alert('broken');
+        // alert('broken');
     }
 });
 
