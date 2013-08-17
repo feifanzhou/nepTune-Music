91c94b5c9c67de636ecb4478c77737fd1ff0ceb3
Feifan Zhou <feifan@me.com>
2013-08-17 12:08:30 -0400

    Display and style "see more" bar at bottom

--- app/assets/javascripts/backbone/routers/app_router.js.jsx
+++ app/assets/javascripts/backbone/routers/app_router.js.jsx
@@ -6,6 +6,9 @@ var AppRouter = Backbone.Router.extend({
 });
 var app_router = new AppRouter;
 app_router.on('route:rootPath', function() {
+	var wh = $(window).height();
+	wh -= 100;
+    $('#mapContainer').attr('style', ('height: ' + wh + 'px;'));
 	// Load Google Maps
 	var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -18,14 +21,19 @@ app_router.on('route:rootPath', function() {
 		{ img_url: 'http://icons.iconarchive.com/icons/walrick/openphone/256/Calendar-icon.png', text: 'Concert tonight' },
 		{ img_url: 'https://cdn0.iconfinder.com/data/icons/cosmo-mobile/40/location_1-128.png', text: 'ThePianoGuys checked in at Five Guys' }
 	];
-    React.renderComponent(
+    React.renderComponent (
     	BurbleBox({ data: data }),
     	document.getElementById('burbleBoxContainer')
+    );
+
+    React.renderComponent (
+    	HomeBottomView({}),
+    	document.getElementById('contentWrapper')
     );
 });
 app_router.on('route:artistPath', function(an) {
 	var artist = new Artist({ artistname: an });
-    React.renderComponent(
+    React.renderComponent (
     	ArtistShowView({ model: artist }),
     	document.getElementById('container')
     );
--- app/assets/javascripts/backbone/views/artists/show.js.jsx
+++ app/assets/javascripts/backbone/views/artists/show.js.jsx
@@ -1,44 +1,55 @@
 /** @jsx React.DOM */
 ArtistShowView = React.createBackboneClass({
-	render: function() {
-		return (<div>{ this.getModel().get('artistname') }</div>);
-	}
+  render: function() {
+    return (<div>{ this.getModel().get('artistname') }</div>);
+  }
 });
 
 BurbleSearch = React.createBackboneClass({
-	render: function() {
-		return (<form id='discoverForm'><input type='text' id='discoverBox' placeholder='Search for artists, events, and music' /></form>);
-	}
+  render: function() {
+    return (<form id='discoverForm'><input type='text' id='discoverBox' placeholder='Search for artists, events, and music' /></form>);
+  }
 });
 BurbleMainFeedItem = React.createBackboneClass({
-	render: function() {
-		return(
-			<li class='BurbleMainFeedItem'>
-				<img class='BurbleFeedProfile' height='40' width='40' src={ this.props.img_url } />
-				{ this.props.children }
-			</li>
-		);
-	}
+  render: function() {
+    return(
+      <li class='BurbleMainFeedItem'>
+        <img class='BurbleFeedProfile' height='40' width='40' src={ this.props.img_url } />
+        { this.props.children }
+      </li>
+    );
+  }
 });
 BurbleMainFeed = React.createBackboneClass({
-	render: function() {
-		var feedNodes = this.props.data.map(function(item) {
-			return <BurbleMainFeedItem img_url={ item.img_url }>{ item.text }</BurbleMainFeedItem>
-		});
-		return (
-			<ol id='mainFeed'>
-				{ feedNodes }
-			</ol>
-		);
-	}
+  render: function() {
+    var feedNodes = this.props.data.map(function(item) {
+      return <BurbleMainFeedItem img_url={ item.img_url }>{ item.text }</BurbleMainFeedItem>
+    });
+    return (
+      <ol id='mainFeed'>
+        { feedNodes }
+      </ol>
+    );
+  }
 });
 BurbleBox = React.createBackboneClass({
-	render: function() {
-		return(
-			<div id='burbleBox'>
-				<BurbleSearch />
-				<BurbleMainFeed data={ this.props.data } />
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div id='burbleBox'>
+        <BurbleSearch />
+        <BurbleMainFeed data={ this.props.data } />
+      </div>
+    );
+  }
 });
+
+HomeBottomView = React.createBackboneClass({
+  render: function() {
+    return (
+      <div id='content'>
+        <p class='Icon' id='scrollDownIndicator'>&#59397;</p>
+        <p>Lorem ipsum son dolore et sit amit</p>
+      </div>
+    )
+  }
+})
