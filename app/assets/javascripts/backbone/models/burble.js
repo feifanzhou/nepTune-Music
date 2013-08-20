f1239a1e7fc311800688fe2591d4c4d1bb9c4c10
Pierre Karashchuk <krchtchk@gmail.com>
2013-08-20 14:18:58 -0400

    better feed backend

--- app/assets/javascripts/backbone/models/burble.js
+++ app/assets/javascripts/backbone/models/burble.js
@@ -1,19 +1,22 @@
 var FeedItem = Backbone.Model.extend({
-	defaults: function() {
-		return {
-			// links: [],
-			// text: '',
-			// type: 'unknown'
-			img_url: '',
-			text: ''
-		};
-	},
-	url: function() {
-		return this.id ? '/burble/home/' + this.id : '/burble/home';
-	}
+    defaults: function() {
+        return {
+            // links: [],
+            // text: '',
+            // type: 'unknown'
+            icon_type: 'img',
+            icon: '',
+            text: '',
+            top_date: '',
+            bottom_date: ''
+        };
+    },
+    url: function() {
+        return this.id ? '/burble/home/' + this.id : '/burble/home';
+    }
 });
 
 var Feed = Backbone.Collection.extend({
-	model: FeedItem,
-	url: '/burble/home'
+    model: FeedItem,
+    url: '/burble/home'
 });
